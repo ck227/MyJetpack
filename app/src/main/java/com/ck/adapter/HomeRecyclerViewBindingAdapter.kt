f697b7bd4615803e766aa4f4c17f95bd19981c32
ck227 <879700945@qq.com>
2020-12-17 19:35:12 +0800

    开始修改网络请求

--- app/src/main/java/com/ck/adapter/HomeRecyclerViewBindingAdapter.kt
+++ app/src/main/java/com/ck/adapter/HomeRecyclerViewBindingAdapter.kt
@@ -17,7 +17,7 @@ import com.ck.myjetpack.R
 fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
     if (!imageUrl.isNullOrEmpty()) {
         Glide.with(view.context)
-            .load(imageUrl)
+            .load("https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2159135328,2589525587&fm=26&gp=0.jpg")
             .transition(DrawableTransitionOptions.withCrossFade())
             .placeholder(R.mipmap.home_title_im_msg_black)
             .error(R.mipmap.home_title_im_msg_black)
